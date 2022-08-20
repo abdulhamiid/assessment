41ee3e0daf91543f75797c7facf3c9409a59f40c
Abdulhamid <adioabdulhamid@yahoo.com>
2022-08-20 12:30:26 +0100

    Fix linters errors

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchLoadedData } from '../redux/loadApi';
-import { States } from './States';
-// import styles from './Home.module.css';
+import States from './States';
 
 let isFetching = true;
 
@@ -11,10 +10,11 @@ function Home() {
   if (isFetching === true) {
     isFetching = false;
     dispatch(fetchLoadedData());
-
   }
   const result = useSelector((state) => state.data);
-  const {death, discharged, totalActiveCases, totalConfirmedCases } = result; 
+  const {
+    death, discharged, totalActiveCases, totalConfirmedCases,
+  } = result;
   return (
     <>
       <div>
@@ -22,21 +22,23 @@ function Home() {
           COVID CASES IN NIGERIA
         </h1>
         {
-          result.length === 0 ? <h1>Loading...</h1> :
-            <>
-              <ul>
-                <li>Deaths</li>
-                <li>Discharged</li>
-                <li>Total Active Cases</li>
-                <li>Total Confirmed Cases</li>
-              </ul>
-              <ul>
-                <li>{death.toLocaleString()}</li>
-                <li>{discharged.toLocaleString()}</li>
-                <li>{totalActiveCases.toLocaleString()}</li>
-                <li>{totalConfirmedCases.toLocaleString()}</li>
-              </ul>
-            </>
+          result.length === 0 ? <h1>Loading...</h1>
+            : (
+              <>
+                <ul>
+                  <li>Deaths</li>
+                  <li>Discharged</li>
+                  <li>Total Active Cases</li>
+                  <li>Total Confirmed Cases</li>
+                </ul>
+                <ul>
+                  <li>{death.toLocaleString()}</li>
+                  <li>{discharged.toLocaleString()}</li>
+                  <li>{totalActiveCases.toLocaleString()}</li>
+                  <li>{totalConfirmedCases.toLocaleString()}</li>
+                </ul>
+              </>
+            )
         }
       </div>
       <States />
--- src/components/views/States.js
+++ src/components/views/States.js
@@ -1,27 +1,28 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link, useParams} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { BsArrowRightCircle } from 'react-icons/bs';
 
-export const States = () => {
+const States = () => {
   const result = useSelector((state) => state.data);
-  const states = result.states;
-  const {stateId} = useParams();
+  const { states } = result;
   return (
     <ul>
       {
-        result.length === 0 ? '':
-        states.map(
-          (state) => (
-            <li key={state.state}>
-              {state.state}
-              <Link to={`/${state.state}`} exact="true">
-                <BsArrowRightCircle />
-              </Link>
-            </li>
+        result.length === 0 ? ''
+          : states.map(
+            (state) => (
+              <li key={state.state}>
+                {state.state}
+                <Link to={`/${state.state}`} exact="true">
+                  <BsArrowRightCircle />
+                </Link>
+              </li>
+            ),
           )
-        )
       }
     </ul>
-  )
-}
+  );
+};
+
+export default States;
