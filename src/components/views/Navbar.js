238cbae4c89823590c90747084c9b501e15756a0
Abdulhamid <adioabdulhamid@yahoo.com>
2022-08-20 14:42:38 +0100

    Style the navbar

--- src/components/views/Navbar.js
+++ src/components/views/Navbar.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ImHome3 } from 'react-icons/im';
 import { FiSettings } from 'react-icons/fi';
+import styles from './Navbar.module.css'
 
 const Navbar = () => (
-  <nav>
+  <nav className={styles.nav}>
     <ul>
       <li>
         <Link to="/" exact="true">
