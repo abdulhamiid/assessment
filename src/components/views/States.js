4ccd0d3e7bdf2635d4b1c4592d9e5272d5c97d46
Abdulhamid <adioabdulhamid@yahoo.com>
2022-08-20 14:23:20 +0100

    Add styling with css modules

--- src/components/views/States.js
+++ src/components/views/States.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { BsArrowRightCircle } from 'react-icons/bs';
+import styles from './States.module.css';
 
 const States = () => {
   const result = useSelector((state) => state.data);
   const { states } = result;
+  console.log(states)
   return (
-    <ul>
+    <section className={styles.sectionTwo}>
+      <h2>States Statistics</h2>
+      <ul className={styles.homepageList}>
+      <li>
+        <p>States</p>
+        <p>Result</p>
+      </li>
       {
         result.length === 0 ? ''
           : states.map(
             (state) => (
               <li key={state.state}>
-                {state.state}
+                <p>{state.state}</p>
                 <Link to={`/${state.state}`} exact="true">
                   <BsArrowRightCircle />
                 </Link>
@@ -22,6 +30,7 @@ const States = () => {
           )
       }
     </ul>
+    </section>
   );
 };
 
