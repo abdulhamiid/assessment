2183823ed3bb2b1feb7d700efd5b53ef9cf08e16
Abdulhamid <adioabdulhamid@yahoo.com>
2022-08-20 14:20:59 +0100

    Create a new component for the showcase area

---
+++ src/components/views/Showcase.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import styles from './Showcase.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchLoadedData } from '../redux/loadApi';
+import flag from './assets/images/flag.png';
+
+const Showcase = () => {
+  let isFetching = true;
+  const dispatch = useDispatch();
+  if (isFetching === true) {
+    isFetching = false;
+    dispatch(fetchLoadedData());
+  }
+  const result = useSelector((state) => state.data);
+  const {
+    death, discharged, totalActiveCases, totalConfirmedCases,
+  } = result;
+  return (
+    <div className={styles.showcase}>
+      <header className={styles.header}>
+        <img src={flag} />
+        <h1>NIGERIA</h1>
+      </header>
+      <p>This data on this website is on the corona virus pandemic in Nigeria showing the statistics for all states.</p>
+      <h2>Country Statistics</h2>
+    {
+      result.length === 0 ? <h1>Loading...</h1>
+        : (
+          <div className={styles.countryStat}>
+            <ul>
+              <li>Deaths</li>
+              <li>Discharged</li>
+              <li>Total Active Cases</li>
+              <li>Total Confirmed Cases</li>
+            </ul>
+            <ul>
+              <li>{death.toLocaleString()}</li>
+              <li>{discharged.toLocaleString()}</li>
+              <li>{totalActiveCases.toLocaleString()}</li>
+              <li>{totalConfirmedCases.toLocaleString()}</li>
+            </ul>
+          </div>
+        )
+    }
+  </div>
+  )
+}
+
+export default Showcase;
