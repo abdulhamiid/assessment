b96db488b2a3c2c2f0da813af7d7c0f4389787bf
Abdulhamid <adioabdulhamid@yahoo.com>
2022-08-21 11:23:37 +0100

    Add new component for wrap cases and nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/views/Home';
-import Cases from './components/views/Cases';
 import NotMatch from './components/views/NotMatch';
+import Record from './components/views/Record';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/:stateId" element={<Cases />} />
+        <Route path="/:stateId" element={<Record />} />
         <Route path="*" element={<NotMatch />} />
       </Routes>
     </>
--- src/components/views/Cases.js
+++ src/components/views/Cases.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import Navbar from './Navbar';
 import styles from './Cases.module.css';
 
 const State = () => {
@@ -15,7 +14,6 @@ const State = () => {
 
   return (
     <>
-      <Navbar />
       {
       result.length === 0 ? ''
         : (
